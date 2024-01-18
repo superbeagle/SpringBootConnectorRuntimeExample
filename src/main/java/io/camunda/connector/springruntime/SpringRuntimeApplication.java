91d44e02e819df89bc84876ac01f1aba56b180ab
Joe Pappas <joe.pappas@me.com>
2024-01-18 12:25:33 -0500

    Initial commit

---
+++ src/main/java/io/camunda/connector/springruntime/SpringRuntimeApplication.java
@@ -0,0 +1,13 @@
+package io.camunda.connector.springruntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRuntimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRuntimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/camunda/connector/springruntime/SpringRuntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.camunda.connector.springruntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRuntimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
